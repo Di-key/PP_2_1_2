3be1536b196bb3f03ad98e6268d33254a5a999ff
Di-key <vfantomkhayv@mail.ru>
2024-01-24 19:25:27 +0300

    commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String message;
+    public Cat() {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
